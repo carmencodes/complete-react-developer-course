11962ee76500886411815a9f8c869ac6f8e6abe7
Carmen Jimenez <carmen@Carmens-MBP.fios-router.home>
2019-12-29 20:50:07 -0500

    Updates

---
+++ precache-manifest.55f34ddfaf6a412f0410ddcce07299e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb105fd17a7bf76a066c3f79466d43e",
+    "url": "/complete-react-developer-course/index.html"
+  },
+  {
+    "revision": "57e0c09a16078d1796d8",
+    "url": "/complete-react-developer-course/static/css/main.5c48cd69.chunk.css"
+  },
+  {
+    "revision": "a0aad4e16871875ff344",
+    "url": "/complete-react-developer-course/static/js/2.6277ae1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/complete-react-developer-course/static/js/2.6277ae1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57e0c09a16078d1796d8",
+    "url": "/complete-react-developer-course/static/js/main.4e0bce29.chunk.js"
+  },
+  {
+    "revision": "b8311943a3e6821077b6",
+    "url": "/complete-react-developer-course/static/js/runtime-main.32e1dd4e.js"
+  }
+]);
