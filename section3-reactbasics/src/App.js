1ab31fa1a2847c0e43967ea2baa45978a921b517
Carmen Jimenez <carmen@Carmens-MBP.fios-router.home>
2019-12-29 19:24:34 -0500

    Added searchField feature

--- section3-reactbasics/src/App.js
+++ section3-reactbasics/src/App.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
-import { CardList} from './components/card-list/card-list.component';
+import React, {
+  Component
+} from 'react';
+import {
+  CardList
+} from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,22 +11,36 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users}));
+      .then(users => this.setState({
+        monsters: users
+      }));
   }
 
   render() {
-    return ( 
-      <div className = 'App' > 
-      <CardList monsters={this.state.monsters} />
-      </div>
-    );
-  }
-}
-export default App;
+
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      );
+
+      return ( 
+        <div className='App'>
+          <input
+            type='search'
+            placeholder='search monsters'
+            onChange={e => this.setState({ searchField: e.target.value })}
+            />
+            <CardList monsters={filteredMonsters} />
+        </div>
+              );
+            }
+          }
+          export default App;
