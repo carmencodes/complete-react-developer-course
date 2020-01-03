86ff4becb0836d01a6affad924b8d63963d99f0b
Carmen Jimenez <carmen@Carmens-MBP.fios-router.home>
2020-01-02 19:47:26 -0500

    Update our menu-items to navigate withRouter HOC

--- section5-routing/src/App.js
+++ section5-routing/src/App.js
@@ -1,46 +1,23 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-const HomePage2 = props => {
-  console.log(props);
-  return (
+const HatsPage = () => (
   <div>
-    <Link to='/topics'>Topics</Link>
     <h1>HATS PAGE</h1>
   </div>
 );
-  };
-
-const HatsPage = props => {
-  console.log(props);
-  return (
-  <div>
-    <Link to='/topics'>Topics</Link>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-  };
-
-  const HatsDetail = props => {
-    console.log(props);
-    return (
-    <div>
-      <Link to='/topics'>Topics</Link>
-      <h1>HATS DETAILS</h1>
-    </div>
-  );
-    };
 
 function App() {
   return(
   <div>
-    <Route exact path='/' component={HomePage2} />
-    <Route exact path='/hats' component={HatsPage} />
-    <Route path='/hats/:id' component={HatsDetail} />
+    <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/shop/hats' component={HatsPage} />
+    </Switch>
   </div>
   );
 }
