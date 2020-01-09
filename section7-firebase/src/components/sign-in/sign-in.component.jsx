43eb6e7a3824d8b095fb05b24d478a18148d7cf6
carmencodes <carmen@Carmens-MBP.fios-router.home>
2020-01-08 19:33:41 -0500

    Added Google Sign in

--- section7-firebase/src/components/sign-in/sign-in.component.jsx
+++ section7-firebase/src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -40,6 +42,10 @@ class SignIn extends React.Component{
                     <FormInput name='password' type='password' value={this.state.password} handleChange={this.handleChange} label='password' required />
 
                     <CustomButton type='submit'> Sign In </CustomButton>
+                    <CustomButton onClick={signInWithGoogle}> 
+                    { ' '}
+                    Sign in with Google { ' '} 
+                    </CustomButton>
                     </form>
             </div>
         );
